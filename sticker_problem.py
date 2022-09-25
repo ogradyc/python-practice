b1ecc13cf93990e0375a3c1b69327d62d5dc170c
ogradyc <15698862+ogradyc@users.noreply.github.com>
2022-09-25 01:28:58 -0600

    Create sticker_problem.py
    This is an example python problem from interview questions. Here is my rough draft solution. 
    basically takes each string and puts them in 2 dictionarys with the letter as the key and the value as the number of letters. Then it iterates through the control string's dict checking each character vs the input string's dict and if the difference between the two is more than the variable how_many_stickers, then it becomes the new how_many_stickers variable value. 
    
    Once done with the iteration, then it prints the result of how many stickers need to be bought, thus solving the problem. 
    I particularly like this solution because I made a function that doesn't hardcode a dictionary and you could do anything else as the control string quite easily if you needed to change it to a different test case. That same function is the one that is used to put the input string into a dict.

---
+++ sticker_problem.py
@@ -0,0 +1,56 @@
+
+# Facebook logo stickers cost $2 each from the company store. I have an idea.
+#I want to cut up the stickers, and use the letters to make other words/phrases
+# A Facebook logo sticker contains only the word "facebook", in all lower case letters.
+#
+# write a function that, given a string consisting of a word or words made up of # of letters from the word "facebook", outputs an integer with the number of # stickers I will need to buy.
+#
+# get_num_stickers("coffee kebab") -> 3
+# get_num_stickers("book") -> 1
+# get_num_stickers("ffacebook") ->2
+#
+# you can assume the input you are passed is valid, that is, does not contain # of any non-"facebook" letters, and the only potential non-letter characters # in the string are spaces. 
+
+STICKER = "facebook"
+input = "ffacebook"
+
+#This function will make sure an input is a string, strip out spaces and ensure it is all lowercase. This will prevent false duplicates
+def lowercase_without_spaces(input):
+    result = strip(input, ' ')
+    result = lowercase(result)
+    return result
+
+
+#This function will build a dictionary with each unique letters. ex {f=1, a=1, c=1, e=1, b=1, o=2, k=1}
+def build_letter_hash(input):
+    clean_sticker = lowercase_without_spaces(input)
+    sticker_hash = {}    #creating empty dictionary
+    for i in len(sticker):
+        key_letter = clean_sticker[i]
+        number_of_letters = sticker_hash[i] + 1
+        sticker_hash[key_letter] = number_of_letters
+    result = sticker_hash
+    print(result)
+    return result
+
+
+    #This function will run over the input and show how many stickers to buy
+    def get_num_stickers(input):
+        how_many_stickers = 0
+        if input == "":
+            result = how_many_stickers
+            return result
+        else:
+            sticker_hash = build_letter_hash(STICKER)
+            clean_input = lowercasewithoutspaces(input)
+            input_hash = build_letter_hash(clean_input)
+            for i in len(clean_input):
+                letter_difference = sticker_hash[clean_input[i]] % input_hash[clean_input[i]] 
+                if letter_difference > how_many_stickers:
+                    how_many_stickers = letter_difference
+            result = how_many_stickers
+            print(result)
+            return result
+
+#add Main function here to run this
+get_num_stickers(input)
