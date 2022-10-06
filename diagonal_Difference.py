3a70d8f134d44f70fd32679774d6d0345df93c83
ogradyc <15698862+ogradyc@users.noreply.github.com>
2022-10-06 04:15:43 -0600

    Update diagonal_Difference.py
    Found the way to make the right diag buy creating a "range list" that I could reverse, and then zipping range_list and range_list[::-1] together.

--- diagonal_Difference.py
+++ diagonal_Difference.py
@@ -7,24 +7,26 @@
 arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
 
 def diagonalDifference(arr):
-    print(arr)
-
-    #print(arr[0] + arr[1] + arr[2])
-
-    #print(reversed(range(len(arr))))
-
-    #print(arr[1][1])
-
-    left_diag = 0
+    range_list = []
     for i in range(len(arr)):
+        range_list = range_list + [i]
+
+    print('left')
+    left_diag = 0
+    for i in range_list:
       print(arr[i][i])
       left_diag = left_diag + arr[i][i]
     
+    print(' ')
+    print('right')
     right_diag = 0
-    for i in reversed(range(len(arr))):
-      print(arr[i][i])
-      right_diag = right_diag + arr[i][i]
+    for i, j in zip(range_list, range_list[::-1]):
+      print(arr[i][j])
+      right_diag = right_diag + arr[i][j]
     
+    print(' ')
     result = abs(left_diag - right_diag)
     print(result)
     return result
+
+diagonalDifference(arr)
